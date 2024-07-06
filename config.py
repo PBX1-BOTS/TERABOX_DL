46bb839221287e085585c69dc6b6f2256959fb54
PBX1-BOTS <bxjjjzid@gmail.com>
2024-07-06 10:48:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 ###❖➥
-API_ID = "123456"  # api id
+API_ID = "20841292"  # api id
 
 ###❖➥
-API_HASH = "ed44780dedd182084f2133b16944cf565"  
+API_HASH = "58c4e84b9b2506684df6447d3e324dd9"  
 
 ###❖➥
 BOT_TOKEN = None
@@ -15,13 +15,14 @@ HOST = "localhost"  # redis host uri
 PORT = "6379"  # redis port
 
 ###❖➥
-PASSWORD = ""  # redis password
+PASSWORD = "PBX1BOTS"  # redis password
 
 ###❖➥
 PRIVATE_CHAT_ID = "-1002010924139"  # CHAT WHERE YOU WANT TO STORE VIDEOS
 
 ###❖➥
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIE = "browserid-09/Muu22YyfpYXePycgyC16RKSomal3nZHAT/KmYDQlEqiQbStjSFFz7M- lang-en TSID-CAHN2U3NnDXcnR7kJkDNzLZdvih1Nao; bid_19072fbdf3653fdcf4207
+ndut_fmt-CFBEFB44CA4FF9B75AFCF5728AF67358C7C5A375FF11283E00F2DA0868380726"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 ###❖➥
-ADMINS = "6922271843"
+ADMINS = "6052303737"
